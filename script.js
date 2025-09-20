6450b242ebeb7cdbba03d7e07a0a5b6ee3c02cf9
yashsaini23 <yashsaini23082005@gmail.com>
2025-09-20 19:07:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 function showMessage() {
     alert("I'm sorry for my behaviour");
 }
-
 document.addEventListener("DOMContentLoaded", function () {
     const greetingCard = document.querySelector(".greeting-card");
     greetingCard.style.transform = "scale(0.95)";
